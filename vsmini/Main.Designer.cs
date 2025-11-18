6471aa305b89e5c2f93e67206aa0c5b3e48b819b
dismal <118924562+Dismalitie@users.noreply.github.com>
2025-11-18 22:10:02 +0000

    update with abysmalcore ace comp

---
+++ vsmini/Main.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace vsmini
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Label entryLabel;
+            Label argsLabel;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            code = new FastColoredTextBoxNS.FastColoredTextBox();
+            asmList = new ListBox();
+            run = new Button();
+            save = new Button();
+            open = new Button();
+            addAsm = new Button();
+            autoAsm = new Button();
+            delAsm = new Button();
+            asmName = new TextBox();
+            saveAs = new Button();
+            switchCompiler = new Button();
+            diagnostics = new RichTextBox();
+            entryMethod = new TextBox();
+            argsBox = new TextBox();
+            delimiter = new TextBox();
+            entryLabel = new Label();
+            argsLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)code).BeginInit();
+            SuspendLayout();
+            // 
+            // entryLabel
+            // 
+            entryLabel.AutoSize = true;
+            entryLabel.Location = new Point(12, 653);
+            entryLabel.Name = "entryLabel";
+            entryLabel.Size = new Size(76, 19);
+            entryLabel.TabIndex = 15;
+            entryLabel.Text = "Entrypoint:";
+            // 
+            // argsLabel
+            // 
+            argsLabel.AutoSize = true;
+            argsLabel.Location = new Point(278, 653);
+            argsLabel.Name = "argsLabel";
+            argsLabel.Size = new Size(80, 19);
+            argsLabel.TabIndex = 17;
+            argsLabel.Text = "Arguments:";
+            // 
+            // code
+            // 
+            code.AutoCompleteBrackets = true;
+            code.AutoCompleteBracketsList = new char[]
+    {
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    '"',
+    '"',
+    '\'',
+    '\''
+    };
+            code.AutoIndentChars = false;
+            code.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;]+);\r\n^\\s*(case|default)\\s*[^:]*(?<range>:)\\s*(?<range>[^;]+);\r\n";
+            code.AutoScrollMinSize = new Size(542, 272);
+            code.BackBrush = null;
+            code.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            code.CharHeight = 17;
+            code.CharWidth = 9;
+            code.DisabledColor = Color.FromArgb(100, 180, 180, 180);
+            code.Font = new Font("Cascadia Code", 9.792F);
+            code.Hotkeys = resources.GetString("code.Hotkeys");
+            code.IsReplaceMode = false;
+            code.Language = FastColoredTextBoxNS.Language.CSharp;
+            code.LeftBracket = '(';
+            code.LeftBracket2 = '{';
+            code.Location = new Point(12, 12);
+            code.Name = "code";
+            code.Paddings = new Padding(0);
+            code.RightBracket = ')';
+            code.RightBracket2 = '}';
+            code.SelectionColor = Color.FromArgb(60, 0, 0, 255);
+            code.ServiceColors = (FastColoredTextBoxNS.ServiceColors)resources.GetObject("code.ServiceColors");
+            code.Size = new Size(942, 631);
+            code.TabIndex = 1;
+            code.Text = resources.GetString("code.Text");
+            code.VirtualSpace = true;
+            code.Zoom = 100;
+            code.TextChanged += code_TextChanged;
+            // 
+            // asmList
+            // 
+            asmList.Font = new Font("Cascadia Code", 9.163636F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            asmList.FormattingEnabled = true;
+            asmList.ItemHeight = 18;
+            asmList.Location = new Point(960, 12);
+            asmList.Name = "asmList";
+            asmList.Size = new Size(178, 634);
+            asmList.TabIndex = 3;
+            asmList.SelectedIndexChanged += asmList_SelectedIndexChanged;
+            // 
+            // run
+            // 
+            run.Location = new Point(960, 713);
+            run.Name = "run";
+            run.Size = new Size(86, 44);
+            run.TabIndex = 5;
+            run.Text = "run";
+            run.UseVisualStyleBackColor = true;
+            run.Click += run_Click;
+            // 
+            // save
+            // 
+            save.Location = new Point(1052, 763);
+            save.Name = "save";
+            save.Size = new Size(86, 26);
+            save.TabIndex = 6;
+            save.Text = "save";
+            save.UseVisualStyleBackColor = true;
+            save.Click += save_Click;
+            // 
+            // open
+            // 
+            open.Location = new Point(960, 763);
+            open.Name = "open";
+            open.Size = new Size(86, 26);
+            open.TabIndex = 7;
+            open.Text = "open";
+            open.UseVisualStyleBackColor = true;
+            open.Click += open_Click;
+            // 
+            // addAsm
+            // 
+            addAsm.Location = new Point(960, 649);
+            addAsm.Name = "addAsm";
+            addAsm.Size = new Size(52, 26);
+            addAsm.TabIndex = 8;
+            addAsm.Text = "+";
+            addAsm.UseVisualStyleBackColor = true;
+            addAsm.Click += addAsm_Click;
+            // 
+            // autoAsm
+            // 
+            autoAsm.Location = new Point(1018, 648);
+            autoAsm.Name = "autoAsm";
+            autoAsm.Size = new Size(62, 26);
+            autoAsm.TabIndex = 9;
+            autoAsm.Text = "auto";
+            autoAsm.UseVisualStyleBackColor = true;
+            autoAsm.Click += autoAsm_Click;
+            // 
+            // delAsm
+            // 
+            delAsm.Location = new Point(1086, 649);
+            delAsm.Name = "delAsm";
+            delAsm.Size = new Size(52, 26);
+            delAsm.TabIndex = 10;
+            delAsm.Text = "-";
+            delAsm.UseVisualStyleBackColor = true;
+            delAsm.Click += delAsm_Click;
+            // 
+            // asmName
+            // 
+            asmName.Location = new Point(960, 681);
+            asmName.Name = "asmName";
+            asmName.Size = new Size(178, 26);
+            asmName.TabIndex = 11;
+            asmName.TextChanged += asmName_TextChanged;
+            // 
+            // saveAs
+            // 
+            saveAs.Location = new Point(960, 795);
+            saveAs.Name = "saveAs";
+            saveAs.Size = new Size(178, 26);
+            saveAs.TabIndex = 12;
+            saveAs.Text = "save as";
+            saveAs.UseVisualStyleBackColor = true;
+            saveAs.Click += saveAs_Click;
+            // 
+            // switchCompiler
+            // 
+            switchCompiler.Font = new Font("Segoe UI", 8F);
+            switchCompiler.Location = new Point(1052, 713);
+            switchCompiler.Name = "switchCompiler";
+            switchCompiler.Size = new Size(86, 44);
+            switchCompiler.TabIndex = 13;
+            switchCompiler.Text = "compiler: ace";
+            switchCompiler.UseVisualStyleBackColor = true;
+            switchCompiler.Click += switchCompiler_Click;
+            // 
+            // diagnostics
+            // 
+            diagnostics.AcceptsTab = true;
+            diagnostics.AutoWordSelection = true;
+            diagnostics.Font = new Font("Cascadia Code", 9.163636F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            diagnostics.Location = new Point(12, 681);
+            diagnostics.Name = "diagnostics";
+            diagnostics.ReadOnly = true;
+            diagnostics.Size = new Size(942, 140);
+            diagnostics.TabIndex = 14;
+            diagnostics.Text = "";
+            // 
+            // entryMethod
+            // 
+            entryMethod.Font = new Font("Cascadia Code", 9.163636F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            entryMethod.Location = new Point(94, 650);
+            entryMethod.Name = "entryMethod";
+            entryMethod.Size = new Size(178, 24);
+            entryMethod.TabIndex = 16;
+            entryMethod.Text = "App.App.Main";
+            // 
+            // argsBox
+            // 
+            argsBox.Font = new Font("Cascadia Code", 9.163636F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            argsBox.Location = new Point(364, 650);
+            argsBox.Name = "argsBox";
+            argsBox.Size = new Size(178, 24);
+            argsBox.TabIndex = 18;
+            argsBox.Text = "Hello|world!";
+            // 
+            // delimiter
+            // 
+            delimiter.Font = new Font("Cascadia Code", 9.163636F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            delimiter.Location = new Point(548, 651);
+            delimiter.MaxLength = 1;
+            delimiter.Name = "delimiter";
+            delimiter.Size = new Size(18, 24);
+            delimiter.TabIndex = 20;
+            delimiter.Text = "|";
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1150, 833);
+            Controls.Add(delimiter);
+            Controls.Add(argsBox);
+            Controls.Add(argsLabel);
+            Controls.Add(entryMethod);
+            Controls.Add(entryLabel);
+            Controls.Add(diagnostics);
+            Controls.Add(switchCompiler);
+            Controls.Add(saveAs);
+            Controls.Add(asmName);
+            Controls.Add(delAsm);
+            Controls.Add(autoAsm);
+            Controls.Add(addAsm);
+            Controls.Add(open);
+            Controls.Add(save);
+            Controls.Add(run);
+            Controls.Add(asmList);
+            Controls.Add(code);
+            Name = "Main";
+            Text = "vsmini*";
+            Load += Main_Load;
+            ((System.ComponentModel.ISupportInitialize)code).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox code;
+        private ListBox asmList;
+        private Button run;
+        private Button save;
+        private Button open;
+        private Button addAsm;
+        private Button autoAsm;
+        private Button delAsm;
+        private TextBox asmName;
+        private Button saveAs;
+        private Button switchCompiler;
+        private RichTextBox diagnostics;
+        private Label entryLabel;
+        private TextBox entryMethod;
+        private TextBox argsBox;
+        private TextBox delimiter;
+    }
+}
---
+++ vsmini/Main.cs
@@ -0,0 +1,310 @@
+﻿using AbysmalCore.Debugging;
+using AbysmalCore.Extensibility;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace vsmini
+{
+    public partial class Main : Form
+    {
+        enum Compiler { csc, ace }
+        static Compiler currentCompiler = Compiler.ace;
+        static string currentFile = "";
+
+        public Main() => InitializeComponent();
+
+        private void switchCompiler_Click(object sender, EventArgs e)
+        {
+            if (currentCompiler == Compiler.csc) currentCompiler = Compiler.ace;
+            else currentCompiler = Compiler.csc;
+
+            switchCompiler.Text = $"compiler: {currentCompiler}";
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            AbysmalDebug.Enabled = false;
+
+            save.Enabled = false;
+            run.Enabled = false;
+            delAsm.Enabled = false;
+            addAsm.Enabled = false;
+
+            Dictionary<Control, string> tooltips = new()
+            {
+                { open, "loads a file to the editor" },
+                { save, "saves the current file" },
+                { saveAs, "saves as a new file" },
+
+                { switchCompiler, "switches the compiler between\ncsc and ace\n" +
+                "csc - cs compiler: up to c# 5, error verbosity\n" +
+                "ace - abysmalcore extensibility: modern, in-mem, more features" },
+
+                { entryMethod, "defines the entrypoint of the app (ace only)" },
+                { argsBox, "command line args to pass" },
+
+                { run, "compiles and runs the code" },
+                { autoAsm, "adds assembly references from the source" },
+                { addAsm, "adds an assembly reference" },
+                { asmName, "assembly reference to add" },
+                { delAsm, "removes the currently selected assembly reference" },
+                { this, "vsmini made by harry" },
+            };
+
+            foreach (KeyValuePair<Control, string> kvp in tooltips)
+            {
+                ToolTip tip = new()
+                {
+                    UseAnimation = true,
+                    UseFading = true
+                };
+                tip.SetToolTip(kvp.Key, kvp.Value);
+            }
+
+            autoAsm_Click(this, null);
+        }
+
+        private void addAsm_Click(object sender, EventArgs e) => asmList.Items.Add(asmName.Text);
+        private void delAsm_Click(object sender, EventArgs e)
+        {
+            asmList.Items.RemoveAt(asmList.SelectedIndex);
+            delAsm.Enabled = false;
+        }
+        private void asmList_SelectedIndexChanged(object sender, EventArgs e) => delAsm.Enabled = true;
+
+        private void asmName_TextChanged(object sender, EventArgs e)
+        {
+            if (asmName.Text != string.Empty)
+                addAsm.Enabled = true;
+        }
+
+        private void autoAsm_Click(object sender, EventArgs e)
+        {
+            List<string> duplicates = new();
+            List<string> unique = new();
+
+            foreach (string line in code.Lines)
+            {
+                if (line.Split(' ')[0] == "using")
+                {
+                    int libIdx = 1;
+                    if (line.Split(' ')[1] == "static") libIdx = 2;
+
+                    string newLib = line.Split(" ")[libIdx].Replace(";", "") + ".dll";
+                    if (asmList.Items.Contains(newLib)) duplicates.Add(newLib);
+                    else unique.Add(newLib);
+                }
+            }
+
+            asmList.Items.AddRange(unique.ToArray());
+            StringBuilder msg = new("[auto assembly] ");
+
+            if (unique.Count > 0)
+            {
+                msg.AppendLine($"included {unique.Count} new libs:");
+                foreach (string x in unique) msg.AppendLine(x);
+                msg.AppendLine();
+            }
+
+            if (duplicates.Count > 0)
+            {
+                msg.AppendLine($"found {duplicates.Count} duplicates:");
+                foreach (string x in duplicates) msg.AppendLine(x);
+            }
+
+            diagnostics.Text = msg.ToString();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new()
+            {
+                Filter = "C# Files|*.cs|Any File|*.*",
+                Title = "Save File"
+            };
+            dlg.ShowDialog();
+
+            if (dlg.FileName == "") return;
+
+            code.Text = File.ReadAllText(dlg.FileName);
+            currentFile = dlg.FileName;
+            autoAsm_Click(null, new());
+
+            run.Enabled = true;
+            Text = "vsmini";
+
+            loadOptions();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(currentFile, code.Text);
+            Text = "vsmini";
+
+            save.Enabled = false;
+            run.Enabled = true;
+        }
+
+        private void code_TextChanged(object sender, FastColoredTextBoxNS.TextChangedEventArgs e)
+        {
+            loadOptions();
+            if (currentFile == "") Text = "vsmini*";
+            else
+            {
+                if (code.Text == File.ReadAllText(currentFile))
+                {
+                    save.Enabled = false;
+                    run.Enabled = true;
+                    Text = "vsmini";
+                }
+                else
+                {
+                    save.Enabled = true;
+                    run.Enabled = false;
+                    Text = "vsmini*";
+                }
+            }
+        }
+
+        private void saveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new()
+            {
+                Filter = "C# Files|*.cs|Any File|*.*",
+                Title = "Save File"
+            };
+            dlg.ShowDialog();
+
+            if (dlg.FileName == "") return;
+
+            File.WriteAllText(dlg.FileName, code.Text);
+            currentFile = dlg.FileName;
+
+            save.Enabled = false;
+            run.Enabled = true;
+            Text = "vsmini";
+        }
+
+        private void loadOptions()
+        {
+            if (!code.Text.StartsWith("//")) return;
+
+            string[] args = string.Concat(code.Text.Split("\n")[0].Skip(2)).Split(',');
+            foreach (string arg in args)
+            {
+                string k = arg.Split(':')[0];
+                string v = arg.Split(':')[1].Trim();
+
+                if (k == "c")
+                {
+                    if (v == "csc") currentCompiler = Compiler.csc;
+                    else currentCompiler = Compiler.ace;
+
+                    switchCompiler_Click(this, null);
+                    switchCompiler_Click(this, null);
+                }
+                else if (k == "ep") entryMethod.Text = v;
+                else if (k == "args") argsBox.Text = v;
+                else if (k == "argd")
+                {
+                    Clipboard.SetText(v);
+                    if (v == "null") delimiter.Text = string.Empty;
+                    else delimiter.Text = v[0].ToString();
+                }
+            }
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            loadOptions();
+            if (currentCompiler == Compiler.csc)
+            {
+                save_Click(null, new());
+                StringBuilder args = new();
+
+                foreach (string dll in asmList.Items) args.Append($"/r:{dll} ");
+                args.Append($"/r:{Path.GetFullPath(".\\AbysmalCore.dll")} ");
+                string exePath = Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileNameWithoutExtension(currentFile)) + ".exe";
+                args.Append($"/out:{exePath} ");
+                args.Append(currentFile);
+
+                diagnostics.Text = "[csc] ";
+                Process compProc = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\csc.exe",
+                    Arguments = args.ToString(),
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                })!;
+                compProc.WaitForExit();
+
+                diagnostics.Text += compProc.StandardOutput.ReadToEnd();
+
+                if (!diagnostics.Text.Contains("error"))
+                {
+                    diagnostics.Text += "[csc] compiled\n\n";
+
+                    string cmdArgs;
+                    if (delimiter.Text == string.Empty) cmdArgs = argsBox.Text;
+                    else cmdArgs = argsBox.Text.Replace(delimiter.Text[0], ' ');
+                    Process appProc = Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = exePath,
+                        Arguments = cmdArgs,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    })!;
+                    appProc.WaitForExit();
+
+                    diagnostics.Text += appProc.StandardOutput.ReadToEnd();
+                }
+            }
+            else
+            {
+                save_Click(null, new());
+
+                string[] output;
+                diagnostics.Text = "[ace] compilation started\n";
+                Assembly? asm = ExtensibilityHelper.CompileAssemblyFromString(code.Text, out output);
+                diagnostics.Text += string.Join("\n", output
+                    .Where(s => !s.Contains("[Hidden]"))
+                    .Select(s => $"[ace:diag]{s}"));
+
+                if (asm != null)
+                {
+                    diagnostics.Text += $"\n[ace] compiled (with {output.Length} diagnostics)\n";
+                    UniformAssembly u = ExtensibilityHelper.LoadAssembly(asm, true);
+
+                    TextWriter oOut = Console.Out;
+                    diagnostics.Text += $"[ace] invoking entrypoint {entryMethod.Text}\n\n";
+                    StringWriter nOut = new StringWriter();
+                    Console.SetOut(nOut);
+
+                    string cls = string.Join('.', entryMethod.Text.Split('.').Take(entryMethod.Text.Split('.').Length - 1));
+                    string method = entryMethod.Text.Split('.').Last();
+                    UniformMethod m = u.Classes[cls].Methods[method];
+
+                    List<string> args = [];
+                    if (delimiter.Text == string.Empty)
+                        foreach (char c in argsBox.Text.ToCharArray()) 
+                            args.Add(c.ToString());
+                    else args = argsBox.Text.Split(delimiter.Text[0]).ToList();
+                    try
+                    {
+                        m.Invoke([args.ToArray()]);
+                    }
+                    catch (Exception ex)
+                    {
+                        diagnostics.Text += $"\n{ex.GetType().Name}: {ex.Message}";
+                    }
+
+                    Console.SetOut(oOut);
+                    diagnostics.Text += nOut.ToString();
+                }
+            }
+        }
+    }
+}
